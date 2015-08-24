3cb3d13a793cfe6021b5ce836b4f000efff7d4d9
Ghirba Cristian <cristianghirba@gmail.com>
2015-08-24 15:47:53 +0300

    third commit

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
  	<link rel="stylesheet" type="text/css" href="css/style.css">
  </head>
  <body>
- 	<h1>The best site was create here!</h1>
- 	<p>
- 		please remeber it
- 	</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
  </body>
  </html>
