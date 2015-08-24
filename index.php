10a87da87150effe0913e1ca95d0f6ec56baf757
Ghirba Cristian <cristianghirba@gmail.com>
2015-08-24 15:42:58 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+	$connection = mysqli_connect('localhost', 'root', 'yo1kl2mn3o4', 'test') or exit('Dont work');
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Main DOC</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+ <body>
+ 	<h1>The best!</h1>
+ </body>
+ </html>
